c3d685fc78c87c415a3bd8d1c3747cfdbf43e0a1
leonardosanches <leo_urania01@hotmail.com>
2020-12-21 17:29:24 -0300

    finishBackEnd

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 //importar as funcionaçodades do express na variavel express
 const { response } = require('express');
 const express = require ('express');
+const cors = require ('cors'); //Módulo de segurança
 const routes = require ('./routes');
 
 //criar aplicação
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
